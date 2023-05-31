28e655115da952056f4cc6e97cd62b850b3ff548
Ye Ou <myeou20@gmail.com>
2023-05-31 17:10:22 -0400

    branch completed

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var moreButtonn = document.getElementById("more-button");
+    var hiddenText = document.getElementById("hidden-text");
+
+    moreBtn.addEventListener("click", function() {
+        hiddenText.style.display = "block";
+    });
+});
